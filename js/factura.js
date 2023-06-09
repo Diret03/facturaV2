493480f0b85d28d8f25abe95a117499aeea58fdd
Diret03 <diegodavidrecalde@gmail.com>
2023-06-09 18:05:52 -0500

    control de filas

--- js/factura.js
+++ js/factura.js
@@ -1,60 +1,34 @@
 
-let cont = 0;  //atributo no usado
-
 let subtotal = 0;
 let valorIva = 0;
 let valorFinal = 0;
 
-function calculateTotal() {  //metodo descartado
-
-  var table = document.getElementById("miTabla");
-  var tbody = table.getElementsByTagName("tbody")[0];
-  var fila = tbody.rows[cont];
-
-  var cantidadInput = fila.querySelector(".cant input");
-  var precioUnitarioInput = fila.querySelector(".vUnitario input");
-  var valorTotalInput = fila.querySelector(".vTotal input");
-
-  var cantidad = parseInt(cantidadInput.value);
-  var precioUnitario = parseFloat(precioUnitarioInput.value);
-
-  var valorTotal = cantidad * precioUnitario;
-  valorTotalInput.value = valorTotal.toFixed(2);
-
-  var ultimaFila = tbody.lastElementChild;
-
-  var nuevaFila = document.createElement("tr");
-  nuevaFila.innerHTML = `
-    <td class="cant"><input type="number"></td>
-    <td class="detalle"><input type="text"></td>
-    <td class="vUnitario"><input type="number"></td>
-    <td class="vTotal"><input type="number" disabled></td>
-    <td class="add"><button onclick="calculateTotal()">+</button></td>
-  `;
-  tbody.appendChild(nuevaFila);
-  cont++;
-  console.log(cont);
-}
-
-
 function calcularTotal(button) {
 
-  var fila = button.closest(".fila");
-  var cantidad = parseFloat(fila.querySelector(".cant input").value);
-  var valorUnitario = parseFloat(fila.querySelector(".vUnitario input").value);
-  var valorTotal = cantidad * valorUnitario;
+  let fila = button.closest(".fila");
+  let inputCantidad = fila.querySelector(".cant input").value;
+  let inputUnitario = fila.querySelector(".vUnitario input").value;
 
-  subtotal += valorTotal;               //se suma y asigna el valor actual del producto a la variable global de subtotal
-  console.log("Subtotal: " + subtotal);
+  if (inputCantidad !== "" && inputCantidad !== "") {
 
-  let inputSubtotal = document.querySelector("#subtotal input");
-  inputSubtotal.value = subtotal;
+    let cantidad = parseFloat(inputCantidad);
+    let valorUnitario = parseFloat(inputUnitario);
+    let valorTotal = cantidad * valorUnitario;
 
-  fila.querySelector(".vTotal input").value = valorTotal.toFixed(2);  //se redondea a dos decimales el valor obtenido
+    subtotal += valorTotal;               //se suma y asigna el valor actual del producto a la variable global de subtotal
+    console.log("Subtotal: " + subtotal);
 
-  ponerFila();
-  calcularIva();
-  calcularTotalFinal();
+    let inputSubtotal = document.querySelector("#subtotal input");
+    inputSubtotal.value = subtotal;
+
+    fila.querySelector(".vTotal input").value = valorTotal.toFixed(2);  //se redondea a dos decimales el valor obtenido
+
+
+    ponerFila();
+    calcularIva();
+    calcularTotalFinal();
+  }
+
 }
 
 function ponerFila() {
@@ -78,12 +52,12 @@ function calcularIva() {
 
   valorIva = subtotal * 0.12;
   let inputIva = document.querySelector("#iva input");
-  inputIva.value = valorIva;
+  inputIva.value = valorIva.toFixed(2);
 }
 
 function calcularTotalFinal() {
 
   valorFinal = subtotal + valorIva;
   let inputFinal = document.querySelector("#total input");
-  inputFinal.value = valorFinal;
+  inputFinal.value = valorFinal.toFixed(2);
 }
